8c16287300fd7922d92676e0762077457d7ed843
divyeshp07 <divyesh5262@gmail.com>
2024-07-17 18:52:34 +0530

    mvc:folders done

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
